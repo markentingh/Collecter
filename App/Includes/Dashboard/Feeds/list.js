7ea7ce90427ddc254171ca83f8b534f675f83f76
markentingh <entingh@gmail.com>
2016-02-10 08:20:20 -0800

    choose feed type & upload documents

--- App/Includes/Dashboard/Feeds/list.js
+++ App/Includes/Dashboard/Feeds/list.js
@@ -3,6 +3,7 @@
         $('#btnaddfeed').off().on('click', S.feeds.buttons.showAddFeed);
         $('#btnsaveaddfeed').off().on('click', S.feeds.buttons.saveAddFeed);
         $('#btncanceladdfeed').off().on('click', S.feeds.buttons.hideAddFeed);
+        $('#lstfeedtype').off().on('change', S.feeds.buttons.changeFeedType);
     },
 
     buttons: {
@@ -23,6 +24,20 @@
             $('#txtaddfeedtitle').val('');
             $('#txtaddfeedurl').val('');
             S.feeds.buttons.hideAddFeed();
+        },
+
+        changeFeedType: function () {
+            var type = $('#lstfeedtype').val();
+            $('.option-feedurl').hide();
+            $('.option-feedupload').hide();
+            switch (type) {
+                case '1': case '2':
+                    $('.option-feedurl').show();
+                    break;
+                case '3':
+                    $('.option-feedupload').show();
+                    break
+            }
         }
     }
 }
---
+++ App/Pages/Upload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Collector.Pages
+{
+    public class Upload : Page
+    {
+        public Upload() : base()
+        {
+        }
+
+        public override string Render()
+        {
+            //check security first
+            if (CheckSecurity() == false) { return RenderAccessDenied(); }
+
+            //setup scaffolding variables
+            Scaffold scaffold = new Scaffold(S, "/app/pages/upload.html", "", new string[] { "type" });
+            scaffold.Data["type"] = S.Request.Query["type"];
+            return scaffold.Render();
+        }
+    }
+}
