e4096d06016b3b01f1fe2e0fe02e94328358d837
Mark Entingh <entingh@easci.com>
2016-02-16 08:36:01 -0800

    add new article fields in database for stats purposes

--- Sql/Stored Procedures/AddArticle.sql
+++ Sql/Stored Procedures/AddArticle.sql
@@ -5,6 +5,13 @@
 	@url nvarchar(250),
 	@title nvarchar(250),
 	@summary nvarchar(250),
+	@wordcount int = 0,
+	@sentencecount int = 0,
+	@paragraphcount int = 0,
+	@importantcount int = 0,
+	@yearstart int = 0,
+	@yearend int = 0,
+	@years nvarchar(50),
 	@images int = 0,
 	@datepublished datetime,
 	@relavance smallint = 1,
@@ -12,7 +19,11 @@
 	@fiction smallint = 1
 AS
 	DECLARE @articleId int = NEXT VALUE FOR SequenceArticles
-	INSERT INTO Articles (articleId, feedId, subjects, domain, url, title, summary, images, datecreated, datepublished, relavance, importance, fiction)
-		VALUES (@articleId, @feedId, @subjects, @domain, @url, @title, @summary, @images, GETDATE(), @datepublished, @relavance, @importance, @fiction)
+	INSERT INTO Articles 
+	(articleId, feedId, subjects, domain, url, title, summary, wordcount, sentencecount, paragraphcount, importantcount, 
+	yearstart, yearend, years, images, datecreated, datepublished, relavance, importance, fiction)
+	VALUES 
+	(@articleId, @feedId, @subjects, @domain, @url, @title, @summary, @wordcount, @sentencecount, @paragraphcount, @importantcount, 
+	@yearstart, @yearend, @years, @images, GETDATE(), @datepublished, @relavance, @importance, @fiction)
+
 	SELECT @articleId
-RETURN 0
--- Sql/Stored Procedures/UpdateArticle.sql
+++ Sql/Stored Procedures/UpdateArticle.sql
@@ -3,12 +3,23 @@
 	@subjects int = 0,
 	@title nvarchar(250),
 	@summary nvarchar(250),
+	@wordcount int = 0,
+	@sentencecount int = 0,
+	@paragraphcount int = 0,
+	@importantcount int = 0,
+	@yearstart int = 0,
+	@yearend int = 0,
+	@years nvarchar(50),
 	@images int = 0,
 	@datepublished datetime,
 	@relavance smallint = 1,
 	@importance smallint = 1,
 	@fiction smallint = 1
 AS
-	UPDATE Articles SET subjects=@subjects, title=@title, summary=@summary, images=@images, datepublished=@datepublished, 
-						relavance=@relavance, importance=@importance, fiction=@fiction
+
+UPDATE Articles SET 
+subjects=@subjects, title=@title, summary=@summary, wordcount=@wordcount, sentencecount=@sentencecount,
+paragraphcount=@paragraphcount, importantcount=@importantcount, yearstart=@yearstart, yearend=@yearend,
+years=@years, images=@images, datepublished=@datepublished, relavance=@relavance, importance=@importance, 
+fiction=@fiction
 WHERE articleId=@articleId
